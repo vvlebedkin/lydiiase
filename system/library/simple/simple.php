dc3d1848fe17338e70ff94ff7f065e77a590486e
Vladislav L <roll26@yandex.ru>
2026-02-02 15:32:22 +0300

    Установил модуль корзины

---
+++ system/library/simple/simple.php
@@ -0,0 +1,18 @@
+<?php
+$php_version = PHP_MAJOR_VERSION . PHP_MINOR_VERSION;
+$ioncube_loaders = array(
+    53 => 53,
+    54 => 54,
+    55 => 55,
+    56 => 56,
+    70 => 56,
+    71 => 71,
+    72 => 72,
+    73 => 72,
+    74 => 72,
+    80 => 80,
+    81 => 81,
+    82 => 82,
+);
+$ioncube_loader = !empty($ioncube_loaders[$php_version]) ? $ioncube_loaders[$php_version] : 72;
+require_once(DIR_SYSTEM . '/library/simple/php/simple_system_' . $ioncube_loader . '.php');
