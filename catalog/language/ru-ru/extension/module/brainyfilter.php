287780dfb66cc855c7ee68aa8e1ff58dc4b881b6
Vladislav L <roll26@yandex.ru>
2026-02-04 21:10:23 +0300

    Установил фильтр

---
+++ catalog/language/ru-ru/extension/module/brainyfilter.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Brainy Filter Pro 5.1.3 OC3, September 18, 2017 / brainyfilter.com 
+ * Copyright 2016-2018 ПЕРЕВОД / oc3x.ru
+ * License: Commercial. Reselling of this software or its derivatives is not allowed. You may use this software for one website ONLY including all its subdomains if the top level domain belongs to you and all subdomains are parts of the same OpenCart store. 
+ * Support: http://support.giantleaplab.com
+ */
+$_['heading_title'] 		= '';
+$_['button_clear']			= 'Очистить';
+$_['default_value_select'] 	= '- Выбрать -';
+$_['price_header']			= 'Цена';
+$_['categories_header']	    = 'Категории';
+$_['min_max']				= 'Мин.- Макс.:';
+$_['reset']					= 'Сбросить все';
+$_['submit']				= 'Применить';
+$_['stock_status']          = 'Состояние на складе';
+$_['manufacturers']         = 'Производители';
+$_['entry_show_more']       = 'Показать больше';
+$_['entry_show_less']       = 'Закрыть окно';
+$_['rating']       			= 'Рейтинг';
+$_['option']       			= 'Опции';
+$_['entry_block_title']	    = 'Расширенный поиск';
+$_['entry_search']	        = 'Ключевые слова';
+$_['text_bf_page_title']	= 'Фильтр товаров';
+$_['empty_slider_value']	= 'Не установлен';
