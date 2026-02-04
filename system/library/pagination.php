2a7aa67476c32b847db6776b1563a70cd5fd714f
Vladislav L <roll26@yandex.ru>
2026-02-04 10:15:44 +0300

    Сделал пагинацию

--- system/library/pagination.php
+++ system/library/pagination.php
@@ -46,15 +46,16 @@ class Pagination {
 
 		$this->url = str_replace('%7Bpage%7D', '{page}', $this->url);
 
-		$output = '<ul class="pagination">';
+		$output = '<ul class="pagin">';
+
+		$svg_prev = '<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1433 21.8237C16.3281 21.8237 16.5172 21.7507 16.659 21.6089C16.9426 21.3253 16.9426 20.8612 16.659 20.5776L6.95234 10.871L16.5172 1.30615C16.8008 1.02256 16.8008 0.558498 16.5172 0.274904C16.2336 -0.00868988 15.7695 -0.00868988 15.4859 0.274904L5.40117 10.3554C5.11758 10.639 5.11758 11.103 5.40117 11.3866L15.6234 21.6089C15.7695 21.755 15.9543 21.8237 16.1433 21.8237Z" fill="black" /></svg>';
+        $svg_next = '<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.85764 21.8237C5.67287 21.8237 5.48379 21.7507 5.34199 21.6089C5.0584 21.3253 5.0584 20.8612 5.34199 20.5776L15.0486 10.871L5.48379 1.30615C5.20019 1.02256 5.20019 0.558498 5.48379 0.274904C5.76738 -0.00868988 6.23144 -0.00868988 6.51504 0.274904L16.5998 10.3554C16.8834 10.639 16.8834 11.103 16.5998 11.3866L6.37754 21.6089C6.23144 21.755 6.0467 21.8237 5.85764 21.8237Z" fill="black" /></svg>';
 
 		if ($page > 1) {
-			$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '?page={page}', '&page={page}'), '', $this->url) . '">' . $this->text_first . '</a></li>';
-			
 			if ($page - 1 === 1) {
-				$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '?page={page}', '&page={page}'), '', $this->url) . '">' . $this->text_prev . '</a></li>';
+				$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '?page={page}', '&page={page}'), '', $this->url) . '">' . $svg_prev . '</a></li>';
 			} else {
-				$output .= '<li><a href="' . str_replace('{page}', $page - 1, $this->url) . '">' . $this->text_prev . '</a></li>';
+				$output .= '<li><a href="' . str_replace('{page}', $page - 1, $this->url) . '">' . $svg_prev . '</a></li>';
 			}
 		}
 
@@ -91,8 +92,7 @@ class Pagination {
 		}
 
 		if ($page < $num_pages) {
-			$output .= '<li><a href="' . str_replace('{page}', $page + 1, $this->url) . '">' . $this->text_next . '</a></li>';
-			$output .= '<li><a href="' . str_replace('{page}', $num_pages, $this->url) . '">' . $this->text_last . '</a></li>';
+			$output .= '<li><a href="' . str_replace('{page}', $page + 1, $this->url) . '">' . $svg_next . '</a></li>';
 		}
 
 		$output .= '</ul>';
